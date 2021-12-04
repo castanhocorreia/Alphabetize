8acf2d79ddd17b34a0f7087c065614865c0dd24b
castanho <castanhocorreia.dev@gmail.com>
2021-12-04 20:40:15 -0300

    Fix bug in empty lines

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,6 @@
 import { commands, ExtensionContext, Range, TextEditor, window } from "vscode";
 
 const activate = (context: ExtensionContext) => {
-  console.log(context);
   const disposable = commands.registerCommand("alphabetize.alphabetize", () => {
     const editor = window.activeTextEditor as TextEditor;
     const { selection } = editor;
@@ -15,6 +14,7 @@ const activate = (context: ExtensionContext) => {
     } else {
       selectionLength = editor.document.lineAt(endPoint).text.length;
       for (let i = startPoint; i <= endPoint; i++) {
+        if (editor.document.lineAt(i).text.trim() == "") continue;
         lines.push(editor.document.lineAt(i).text);
       }
     }
@@ -23,9 +23,9 @@ const activate = (context: ExtensionContext) => {
       selection.isSingleLine
         ? editBuilder.replace(selection, lines.join(", "))
         : editBuilder.replace(
-            new Range(startPoint, 0, endPoint, selectionLength),
-            lines.join("\n")
-          );
+          new Range(startPoint, 0, endPoint, selectionLength),
+          lines.join("\n")
+        );
     });
   });
   context.subscriptions.push(disposable);
