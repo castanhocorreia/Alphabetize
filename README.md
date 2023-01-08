bcac64b948656e4720e7bf9afdde8bc778123e97
João Correia <me@castanhocorreia.com>
2023-01-08 15:36:21 -0300

    Refactor alphabetize function to use helper function for sorting and replacing selection

---
+++ README.md
@@ -0,0 +1,20 @@
+# Alphabetize
+
+Alphabetize is a Visual Studio Code extension that allows you to sort lines of text alphabetically
+
+## Installation
+
+- Open Visual Studio Code
+- Press `Ctrl+Shift+X` or `Cmd+Shift+X` to open the Extensions pane
+- Search for "Alphabetize"
+- Click the Install button to install the extension
+
+## Usage
+
+- Select the lines of text you want to sort
+- Press `Ctrl+Shift+P` or `Cmd+Shift+P` to open the Command Palette
+- Type "Alphabetize" and select the "Alphabetize" command
+
+- License
+
+Alphabetize is licensed under the [GNU General Public License v3.0](https://www.gnu.org/licenses/gpl-3.0.en.html)
--- src/extension.ts
+++ src/extension.ts
@@ -1,34 +1,31 @@
 import { commands, ExtensionContext, Range, TextEditor, window } from "vscode";
 
-const activate = (context: ExtensionContext) => {
-  const disposable = commands.registerCommand("alphabetize.alphabetize", () => {
-    const editor = window.activeTextEditor as TextEditor;
-    const { selection } = editor;
-    const startPoint = selection.start.line;
-    const endPoint = selection.end.line;
-    let lines: string[] = [];
-    let selectionLength: number;
-    if (selection.isSingleLine) {
-      selectionLength = editor.document.getText(selection).length;
-      lines = editor.document.getText(selection).replace(/\s/g, "").split(",");
-    } else {
-      selectionLength = editor.document.lineAt(endPoint).text.length;
-      for (let i = startPoint; i <= endPoint; i++) {
-        if (editor.document.lineAt(i).text.trim() == "") continue;
-        lines.push(editor.document.lineAt(i).text);
-      }
+const sort = (editor: TextEditor, selection: Range): void => {
+  let lines: string[] = [];
+  if (selection.isSingleLine) {
+    const text = editor.document.getText(selection).trim();
+    lines = text.split(",").map((line) => line.trim());
+  } else {
+    for (let i = selection.start.line; i <= selection.end.line; i++) {
+      const line = editor.document.lineAt(i).text.trim();
+      if (line === "") continue;
+      lines.push(line);
     }
-    lines.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
-    editor.edit((editBuilder) => {
-      selection.isSingleLine
-        ? editBuilder.replace(selection, lines.join(", "))
-        : editBuilder.replace(
-          new Range(startPoint, 0, endPoint, selectionLength),
-          lines.join("\n")
-        );
-    });
+  }
+  lines.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+  const replacement = selection.isSingleLine ? lines.join(", ") : lines.join("\n");
+  editor.edit((editBuilder) => {
+    editBuilder.replace(selection, replacement);
+  })
+}
+
+const alphabetize = (context: ExtensionContext): void => {
+  const disposable = commands.registerCommand("alphabetize", () => {
+    const editor = window.activeTextEditor as TextEditor;
+    const selection = editor.selection;
+    sort(editor, selection);
   });
   context.subscriptions.push(disposable);
-};
+}
 
-exports.activate = activate;
+exports.activate = alphabetize;
